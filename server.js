4f7ee4a3f0342db79101c1352e1a29d19e9c4542
Carlos Ramírez Suárez <cramirez@zemsania.com>
2016-12-02 13:53:47 +0100

    create user and adding cors

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
+var cors = require('cors');
+
+
+
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
+
+
     
 // =======================
 // configuration =========
@@ -18,26 +24,32 @@ var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.use(cors()); 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
 
 // =======================
 // routes ================
-app.get('/setup', function(req, res) {
+app.post('/setup', function(req, res) {
 
   // create a sample user
-  var nick = new User({ 
-    name: 'Nick Cerminara', 
-    password: 'password',
-    admin: true 
-  });
 
+   var newUser = new User();
+    newUser.name = req.body.name;
+    newUser.password =req.body.password;
   // save the sample user
-  nick.save(function(err) {
+  newUser.save(function(err) {
     if (err) throw err;
 
     console.log('User saved successfully');
@@ -124,6 +136,7 @@ apiRoutes.get("/users", function(req, res){
 });
 
 app.use("/api", apiRoutes);
+
 
 
 // =======================
